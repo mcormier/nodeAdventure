cec1ff20e2371cfd37de074adc8a12fee4b757f1
Matthieu Cormier <mcormier@cs.dal.ca>
2014-11-22 09:30:07 -0400

    attach socket.io to server

--- server.js
+++ server.js
@@ -159,12 +159,21 @@ var SampleApp = function() {
 //console.log(process.env);
 
 
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+if (typeof ipaddress === "undefined") {
+   console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+  ipaddress = "127.0.0.1";
+};
+ 
+var http = require('http');
 
-/*
-var io      = require('socket.io')(8000);
+var app = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(port, ipaddress);
 
-
-
+var io  = require('socket.io')(app);
 
 io.on('connection', function (socket) {
   //io.emit('this', { will: 'be received by everyone'});
@@ -181,19 +190,4 @@ io.on('connection', function (socket) {
 
 });
 
-*/
-
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-if (typeof ipaddress === "undefined") {
-   console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-  ipaddress = "127.0.0.1";
-};
- 
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(port, ipaddress);
 
