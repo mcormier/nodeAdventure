128ebe19b2a462c6bb262f38a313b81f5ba40346
Matthieu Cormier <mcormier@cs.dal.ca>
2014-11-25 23:00:08 -0400

    create all rooms for dark secret

--- darksecret.js
+++ darksecret.js
@@ -30,9 +30,40 @@ function ROOMS() {
       short: "The front step.",
       long: "You are on the front step of the house.",
       items: [],
-      exits: [null, null, null, "road"]
+      exits: [null, null, "wood shed", "road"]
+    },
+    "foyer": {
+      short: "The foyer of the house.",
+      long: "You are in the tastefully decorated foyer of the house.",
+      items: [],
+      exits: [null, null, null, "porch"]
+    },
+    "wood shed": {
+      short: "A wood shed",
+      long: "Your are at the SW corner of the house, near the wood shed.",
+      items: [],
+      exits: ["trampoline", "porch", null, null]
+    },
+    "trampoline": {
+      short: "In the backyard by the trampoline.",
+      long: "In the NW corner of the backyard by the trampoline. A zipline runs East "
+           +"to the NE corner of the backyard.",
+      items: [],
+      exits: [null, "muddy yard", null, "wood shed"]
+    },
+    "muddy yard": {
+      short: "In a very muddy yard.",
+      long: "In the backyard of the house, standing up to your ankles in mud. You can't go any further",
+      items: [],
+      exits: [null, null, "trampoline", null ]
+    },
+    "garden": {
+      short: "In a small garden in the backyard.",
+      long: "In the NE corner of the backyard. There is a lovely vegetable garden here."+
+            " A zipline runs West to the NW corner of the backyard.",
+      items: [],
+      exits: [null, null, null, null ]
     }
-
   };
 }
 
